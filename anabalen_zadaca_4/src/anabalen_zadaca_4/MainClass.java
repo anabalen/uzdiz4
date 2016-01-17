c529b20a0d909fb09627d26241f8d10215f72c9c
anabalen <balen.anamarija@gmail.com>
2016-01-17 11:42:34 +0100

    First commit

---
+++ anabalen_zadaca_4/src/anabalen_zadaca_4/MainClass.java
@@ -0,0 +1,16 @@
+package anabalen_zadaca_4;
+
+/**
+ *
+ * @author Ana-Marija
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
